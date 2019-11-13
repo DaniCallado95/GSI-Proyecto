fb0598b3b03b1b115d1b3c75c918cd0e8d942b9c
Dani <danicorella95@gmail.com>
2019-11-13 12:41:36 +0100

    added django suit

--- GestionConsumo/gestionconsumo/models.py
+++ GestionConsumo/gestionconsumo/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.utils import timezone
 
+roles = (
+            ('ad', 'admin'),
+            ('añ', 'añadir'),
+            ('ed', 'editar')
+    )
+
 class Usuario(models.Model):
-    
+
     empresa = models.ForeignKey("Empresa", on_delete=models.CASCADE) 
     username = models.CharField(max_length=50,primary_key=True) 
     password = models.CharField(max_length=50)
-    roll = models.CharField(max_length=10)
+    rol = models.CharField(max_length=10, choices=roles, default='añ')
     created_date = models.DateTimeField(default=timezone.now)
+    
     def __str__(self): 
         return self.username
 
